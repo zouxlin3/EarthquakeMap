58683f5e5fc80b24ff8cf671a2634cd607816d04
zouxlin3 <zouxlin3@qq.com>
2021-06-18 18:36:06 +0800

    frontend:new：earthquakeList card

---
+++ frontend/earthquake_map/src/components/Info.vue
@@ -0,0 +1,19 @@
+<template>
+    <el-card shadow="hover">
+        <el-row type="flex" align="middle">
+            <el-col :span="6">
+                <p>{{ earthquake['mag'] }}</p>
+            </el-col>
+            <el-col :span="18">
+                <p>({{ earthquake['latitude'] }},{{ earthquake['longitude'] }})</p>
+                <p>{{ earthquake['time'].slice(0,10)+' '+earthquake['time'].slice(11,19) }}</p>
+            </el-col>
+        </el-row>
+    </el-card>
+</template>
+
+<script>
+export default {
+    props:['earthquake']
+}
+</script>
---
+++ frontend/earthquake_map/src/components/List.vue
@@ -0,0 +1,20 @@
+<template>
+    <el-card class="box-class">
+        <li v-for="earthquake in earthquakeList" :key="earthquake">
+            <Info :earthquake="earthquake" />
+        </li>
+    </el-card>
+</template>
+
+<script>
+import Info from './Info.vue'
+
+export default {
+    name: 'List',
+    components: {
+        Info
+    },
+
+    props:['earthquakeList']
+}
+</script>
